6ead249b8f84039f532595d74ae7604cf9d995db
cfgs <cfgs@pc-info-host>
2024-01-08 21:15:25 +0100

    Shipment.java: Sender & Recipient changed

--- shipmentsws/src/main/java/cat/institutmarianao/shipmentsws/model/Shipment.java
+++ shipmentsws/src/main/java/cat/institutmarianao/shipmentsws/model/Shipment.java
@@ -11,10 +11,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -67,9 +69,11 @@ public class Shipment implements Serializable {
 	@Column(nullable = false)
 	private Category category;
 
+	@ManyToOne(fetch = FetchType.EAGER, optional = false)
 	@JoinColumn(name = "sender_id", referencedColumnName = "id", nullable = false)
 	private Address sender;
 
+	@ManyToOne(fetch = FetchType.EAGER, optional = false)
 	@JoinColumn(name = "recipient_id", referencedColumnName = "id", nullable = false)
 	private Address recipient;
 
